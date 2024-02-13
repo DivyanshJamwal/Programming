3dbe68afd32f1c3dd268d8bd351d91a57294870c
Coder-X <divyanshsingh1754@gmail.com>
2024-02-13 10:55:32 +0530

    c

---
+++ FullStack/NodeJs/loops.js
@@ -0,0 +1,52 @@
+// for loop
+// console.log(i);
+// for(i=0;i<5;i++)
+// {
+//     console.log("hello: " + [i]);
+// }
+// var i = 10;
+// console.log(i);
+
+
+// while loop
+// a=6
+// while(a>1){
+//     console.log("I am printing -" + a);
+//     a--;
+// }
+// console.log(a);
+
+
+// do-while loop
+// a=6
+// do{
+//     console.log("I am printing -" + a);
+//        a--;
+// }
+// while(a>1)
+// console.log(a);
+
+
+//Arrays
+// a= [1,2,3,4,'hello',null,true,{}];
+// const b = Array(1,2,3,4,'hello',null,undefined);
+// console.log(a);
+// console.log(b);
+// console.table(a);
+// console.table(b);
+
+// for(x in a){
+//     console.log(x);
+// }
+
+// for(x of a){
+//     console.log(x);
+// }
+
+
+//count
+// a=1;
+// b=2;
+// console.count("The value of a is: " +a);
+// console.count("The value of a is: " +a);
+// console.count("The value of b is: " +b);
